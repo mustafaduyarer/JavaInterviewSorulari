0e4118c085d289cf9565b0770756d410bf9e8fc2
mustafa duyarer <mustafaduyarer@gmail.com>
2022-03-12 01:06:10 +0100

    V001

--- src/test/java/javaInterviewSorulari/reverseInteger.java
+++ src/test/java/javaInterviewSorulari/reverseInteger.java
@@ -3,8 +3,7 @@ package javaInterviewSorulari;
 public class reverseInteger {
     public static void main(String[] args) {
         
-        //deneme
-        //deneme2
+       //kodlar buraya yazilacak
 
     }
 }
